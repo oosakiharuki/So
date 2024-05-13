6888c92541cd1a9e5640fef5a22200095a8ac696
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-05-13 13:33:56 +0900

    03_03まで(UIまで)

--- Assets/CameManagerScript.cs
+++ Assets/CameManagerScript.cs
@@ -8,6 +8,12 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject playerPrefab;
 
+    public GameObject boxPrefab;
+
+    public GameObject goalPrefab;
+
+    public GameObject clearText;
+
     int[,] map = { };
 
     GameObject[,] field;
@@ -41,44 +47,76 @@ public class NewBehaviourScript : MonoBehaviour
     //    Debug.Log(debugText);
     //}
 
-    //private Vector2Int GetPlayerIndex()
-    //{
-    //    for (int y = 0; y < map.Length; y++)
-    //    {
-    //        for (int x = 0; x < map.Length; x++)
-    //        {
-    //            if (field[y,x].tag == null)
-    //            {
+private 
+    Vector2Int GetPlayerIndex()
+    {
+        for (int y = 0; y < field.GetLength(0); y++)
+        {
+            for (int x = 0; x < field.GetLength(1); x++)
+            {
+                if (field[y, x] == null)
+                {
+                    continue;
+                }
+                if (field[y, x].tag == "Player")
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+        return new Vector2Int(-1, -1);
+    }
 
-    //            }
-    //            if (field[y,x].tag == "Player")
-    //            {
-    //                return new Vector2Int(x,y);
-    //            }
-    //        }
-    //    }
-    //    return new Vector2Int(-1, -1);
-    //}
+    bool MoveNumber(Vector2Int moveFrom, Vector2Int moveTo)
+    {
+        if (moveTo.y < 0 || moveTo.y >= field.GetLength(0))
+        {
+            return false;
+        }
+        if (moveTo.x < 0 || moveTo.x >= field.GetLength(1))
+        {
+            return false;
+        }
+        if (field[moveTo.y,moveTo.x] != null && field[moveTo.y,moveTo.x].tag == "Box")
+        {
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber(moveTo, moveTo + velocity);
+            if (!success)
+            {
+                return false;
+            }
+        }
 
-    //bool MoveNumber(string player, Vector2Int moveFrom, Vector2Int moveTo)
-    //{
-    //    if (moveTo < 0 || moveTo >= map.Length)
-    //    {
-    //        return false;
-    //    }
-    //    //if (map[moveTo] == 2)
-    //    //{
-    //    //    int velocity = moveTo - moveFrom;
-    //    //    bool success = MoveNumber(2, moveTo, moveTo + velocity);
-    //    //    if (!success)
-    //    //    {
-    //    //        return false;
-    //    //    }
-    //    //}
-    //    field[moveTo] = number;
-    //    field[moveFrom] = null;
-    //    return true;
-    //}
+        field[moveTo.y,moveTo.x] = field[moveFrom.y, moveFrom.x];
+        field[moveFrom.y, moveFrom.x].transform.position = new Vector3(moveTo.x, map.GetLength(0) - moveTo.y, 0);
+        field[moveFrom.y, moveFrom.x] = null;
+        return true;
+    }
+    bool IsCleard()
+    {
+        List<Vector2Int> goals = new List<Vector2Int>();
+        for (int y = 0;y < map.GetLength(0); y++)
+        {
+            for (int x = 0;x < map.GetLength(1); x++)
+            {
+                if (map[y,x] == 3)
+                {
+
+                    goals.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+
+        for(int i = 0;i < goals.Count; i++)
+        {
+            GameObject f = field[goals[i].y, goals[i].x];
+            if(f == null || f.tag != "Box")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -87,8 +125,10 @@ public class NewBehaviourScript : MonoBehaviour
         //PrintArray();
 
         map = new int[,] {
-            {1,0,0,0,0},
             {0,0,0,0,0},
+            {0,3,0,3,0},
+            {0,1,2,0,0},
+            {0,2,3,2,0},
             {0,0,0,0,0}
         };
 
@@ -115,31 +155,54 @@ public class NewBehaviourScript : MonoBehaviour
                         Quaternion.identity
                         );
                 }
+                if (map[y,x] == 2)
+                {
+                    field[y, x] = Instantiate(
+                        boxPrefab,
+                        new Vector3(x, map.GetLength(0) - y, 0),
+                        Quaternion.identity
+                        );
+                }
+
+
+
             }
             debugText += "\n";
         }
         Debug.Log(debugText);
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, -1));
+            //PrintArray();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(0, 1));
+            //PrintArray();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(1, 0));
+            //PrintArray();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector2Int playerIndex = GetPlayerIndex();
+            MoveNumber(playerIndex, playerIndex + new Vector2Int(-1, 0));
+            //PrintArray();
+        }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.RightArrow))
-    //    {
-    //        int playerIndex = GetPlayerIndex();
-
-    //        MoveNumber(1, playerIndex, playerIndex + 1);
-    //        PrintArray();
-
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.LeftArrow))
-    //    {
-    //        int playerIndex = GetPlayerIndex();
-
-    //        MoveNumber(1, playerIndex, playerIndex - 1);
-    //        PrintArray();
-
-    //    }
-    //}
+        if(IsCleard())
+        {
+            clearText.SetActive(true);
+        }
+    }
 }
